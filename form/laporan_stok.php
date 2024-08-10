4c2a33ef60a7c9c9effb1484ccf0ff7c4d51e511
Nqadhafi <qadhafi817@gmail.com>
2024-08-10 12:01:57 +0700

    revisi laporan

--- form/laporan_stok.php
+++ form/laporan_stok.php
@@ -10,7 +10,6 @@ include '../config.php';
 $sql = "SELECT * FROM barang";
 $barangs = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-// Inisialisasi DOMPDF
 $options = new Options();
 $options->set('isRemoteEnabled', true);
 $dompdf = new Dompdf($options);
@@ -20,22 +19,42 @@ $html = '<h1 style="text-align: center;">Laporan Stok Barang</h1>';
 $html .= '<table border="1" width="100%" style="border-collapse: collapse; text-align: center;">';
 $html .= '<thead>';
 $html .= '<tr>';
+$html .= '<th>No</th>';
 $html .= '<th>ID Barang</th>';
 $html .= '<th>Nama Barang</th>';
-$html .= '<th>Stok</th>';
-$html .= '<th>Stock Awal</th>';
-$html .= '<th>Rata-rata</th>';
+$html .= '<th>Stok Awal</th>';
+$html .= '<th>Stok Masuk</th>';
+$html .= '<th>Stok Keluar</th>';
+$html .= '<th>Stok Akhir</th>';
 $html .= '</tr>';
 $html .= '</thead>';
 $html .= '<tbody>';
 
+$no = 1;
 foreach ($barangs as $barang) {
+    // Menghitung stok masuk untuk barang ini
+    $sqlMasuk = "SELECT SUM(jumlah) as total_masuk FROM barang_masuk WHERE idbrg = ?";
+    $stmtMasuk = $pdo->prepare($sqlMasuk);
+    $stmtMasuk->execute([$barang['idbrg']]);
+    $stokMasuk = $stmtMasuk->fetch(PDO::FETCH_ASSOC)['total_masuk'] ?: 0;
+
+    // Menghitung stok keluar untuk barang ini
+    $sqlKeluar = "SELECT SUM(jumlah) as total_keluar FROM barang_keluar WHERE idbrg = ?";
+    $stmtKeluar = $pdo->prepare($sqlKeluar);
+    $stmtKeluar->execute([$barang['idbrg']]);
+    $stokKeluar = $stmtKeluar->fetch(PDO::FETCH_ASSOC)['total_keluar'] ?: 0;
+
+    // Menghitung stok akhir
+    $stokAkhir = $barang['stock_awal'] + $stokMasuk - $stokKeluar;
+
     $html .= '<tr>';
+    $html .= '<td>' . $no++ . '</td>';
     $html .= '<td>' . htmlspecialchars($barang['idbrg']) . '</td>';
     $html .= '<td>' . htmlspecialchars($barang['namabrg']) . '</td>';
-    $html .= '<td>' . htmlspecialchars($barang['stok']) . '</td>';
     $html .= '<td>' . htmlspecialchars($barang['stock_awal']) . '</td>';
-    $html .= '<td>' . htmlspecialchars($barang['rata_rata']) . '</td>';
+    $html .= '<td>' . htmlspecialchars($stokMasuk) . '</td>';
+    $html .= '<td>' . htmlspecialchars($stokKeluar) . '</td>';
+    $html .= '<td>' . htmlspecialchars($stokAkhir) . '</td>';
     $html .= '</tr>';
 }
 
